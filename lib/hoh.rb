7ee3a03a05eff288ffe90b8d1d5c1e4fd2d8132f
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-14 23:09:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
  
 def naming_system
   hash = {
-  :PORTION_1 = {
+  :PORTION_1 => {
    label: "Kingdom",
    sub_category: {
      label: "Phylum",
@@ -41,18 +41,18 @@ def naming_system
    }
  }
  
- :PORTION_2 = {
+ :PORTION_2 => {
    label: "Order"
  }
  
- :PORTION_3 = {
+ :PORTION_3 => {
    label: "Family",
    sub_category: {
      label: "Genus",
    }
  }
  
- :PORTION_4 = {
+ :PORTION_4 => {
    label: "Species",
    sub_category: nil
  }
